8a45685a5b91d5bf7e9ff583eee98d0ca144db8b
Madeline Conway <conway.maddy@yahoo.com>
2024-08-27 14:21:07 -0400

    completed

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callbackFunction){
+    callbackFunction() 
+}
+
+function returnsANamedFunction(){
+    return returnsANamedFunction;
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+};
